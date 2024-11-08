597916e2717350b2e8add8f56dcaf00fbb63505e
Ana Hobden <operator@hoverbear.org>
2024-11-08 12:30:23 -0800

    Allow unbound var

--- ferrocene/ci/scripts/setup-venv.sh
+++ ferrocene/ci/scripts/setup-venv.sh
@@ -7,9 +7,12 @@ set -xeuo pipefail
 curl -LsSf https://astral.sh/uv/0.5.0/install.sh | sh
 
 export PATH="$HOME/.local/bin:$PATH"
-if [ -z ${BASH_ENV+x} ]; then
+
+set +e
+if [ ! -z ${BASH_ENV+x} ]; then
     echo 'export PATH="$HOME/.local/bin:$PATH"' >> $BASH_ENV
 fi
+set -e
 
 # Use the full path here since Windows might not have it on PATH yet
 uv venv
