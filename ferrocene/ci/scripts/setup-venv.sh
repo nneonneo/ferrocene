7718302149f91b6a98a5ac20cd06e3cad517fd3e
Ana Hobden <operator@hoverbear.org>
2024-11-08 10:53:59 -0800

    Wiggle how we use uv on windows a bit

--- ferrocene/ci/scripts/setup-venv.sh
+++ ferrocene/ci/scripts/setup-venv.sh
@@ -4,16 +4,13 @@
 
 set -xeuo pipefail
 
-if command -v uv &> /dev/null; then
-    echo "uv was already installed, skipping"
-    exit 0
-fi
-
 curl -LsSf https://astral.sh/uv/0.5.0/install.sh | sh
 
+export PATH="$PATH:$HOME/.local/bin"
 echo 'export PATH="$PATH:$HOME/.local/bin"' >> $BASH_ENV
 
-uv venv
-uv python install 3.12
-uv python pin 3.12
-uv pip sync requirements.txt
+# Use the full path here since Windows might not have it on PATH yet
+$HOME/.local/bin/uv venv
+$HOME/.local/bin/uv python install 3.12
+$HOME/.local/bin/uv python pin 3.12
+$HOME/.local/bin/uv pip sync requirements.txt
