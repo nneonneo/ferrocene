788a45a858faf2c5ac4d03f4b914e115b4cfb3b9
Ana Hobden <operator@hoverbear.org>
2024-11-08 11:31:00 -0800

    Sometimes BASH_ENV is not set

--- ferrocene/ci/scripts/setup-venv.sh
+++ ferrocene/ci/scripts/setup-venv.sh
@@ -6,8 +6,10 @@ set -xeuo pipefail
 
 curl -LsSf https://astral.sh/uv/0.5.0/install.sh | sh
 
-source $HOME/.local/bin/env
-echo 'source $HOME/.local/bin/env' >> $BASH_ENV
+export PATH="$HOME/.local/bin:$PATH"
+if [ -z ${var+x} ]; then
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> $BASH_ENV
+fi
 
 # Use the full path here since Windows might not have it on PATH yet
 uv venv
